f22607d26714d5e80bf9894f69435c7708ddd24f
JonathasAbalista <jonathasguilherme40@gmail.com>
2025-03-13 15:35:27 -0300

    Casos de teste Real World App

---
+++ cypress/tests/e2e/cadastro.spec.js
@@ -0,0 +1,25 @@
+describe('Registro de novo usuário com sucesso', () => {
+    it('Deve registrar um novo usuário com informações válidas', () => {
+      cy.visit('http://localhost:3000/signup');
+      cy.get('#firstName').type('João');
+      cy.get('#lastName').type('Silva');
+      cy.get('#username').type('joaosilva');
+      cy.get('#password').type('test');
+      cy.get('#confirmPassword').type('test');
+      cy.get('[data-test="signup-submit"]').click();
+    });
+    
+  });
+git 
+  describe('Tentar registrar um novo usuário com informações incompletas', () => {
+    it('Deve exibir mensagens de erro ao tentar registrar um novo usuário sem preencher todas as informações obrigatórias', () => {
+        cy.visit('http://localhost:3000/signup');
+        cy.get('#firstName').type('João');
+        cy.get('#lastName').type('Silva');
+        cy.get('#username').type('joaosilva');
+        cy.get('#password').type('test');
+        cy.get('#confirmPassword').type('  ');
+        
+        
+    });
+  });
---
+++ cypress/tests/e2e/login.spec.js
@@ -0,0 +1,20 @@
+ describe('Login com sucesso', () => {
+    it('Deve fazer login com um usuário válido', () => {
+      cy.visit('http://localhost:3000/login');
+      cy.get('#username').type('jonathasguilherme');
+      cy.get('#password').type('test');
+      cy.get('[data-test="signin-submit"]').click();
+     });
+      
+      
+          
+  });
+
+describe('Tentar fazer login com credenciais inválidas', () => {
+    it('Deve exibir uma mensagem de erro ao fazer login com credenciais inválidas', () => {
+        cy.visit('http://localhost:3000/login');
+        cy.get('#username').type('jonathasguilherme');
+        cy.get('#password').type('test1234');
+        cy.get('[data-test="signin-submit"]').click();
+    });
+});
